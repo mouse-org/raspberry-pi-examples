ce658362a445ce2333868253cf3a8bfa44cabcb5
Patrick Weaver <pjpwea@gmail.com>
2017-10-30 15:33:32 -0400

    make comments shorter, fix typo

--- blink.py
+++ blink.py
@@ -1,9 +1,11 @@
 # gpiozero and time are two python libraries we will use
-# We are importing the LED() function from gpiozero and the sleep() function from time
+# We are importing the LED() function from gpiozero
+# and the sleep() function from time
 from gpiozero import LED
 from time import sleep
 
-# We set the variable led to correspond to GPIO Pin 14 using the LED() function
+# We set the variable led to correspond to
+# GPIO Pin 14 using the LED() function
 led = LED(14)
 
 # This loop runs forever
@@ -14,5 +16,5 @@ while True:
     sleep(1)
     # Turn the LED off
     led.off()
-    # Puase for 1 second
+    # Pause for 1 second
     sleep(1)
